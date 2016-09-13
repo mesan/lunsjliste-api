7c1542d0388fd8734e3e3be607d485b7ca3e635e
Anders Ullnæss <andersu@mesan.no>
2016-09-13 20:18:53 +0200

    Implementerte lagring til Firebase

--- src/main/java/no/mesan/lunsjliste/Application.java
+++ src/main/java/no/mesan/lunsjliste/Application.java
@@ -1,9 +1,14 @@
 package no.mesan.lunsjliste;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 @Configuration
 @EnableAutoConfiguration
@@ -12,5 +17,15 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
+        try {
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setServiceAccount(new FileInputStream("lunchlist-mesan-4bb87ab49910.json"))
+                    .setDatabaseUrl("https://lunchlist-mesan.firebaseio.com/")
+                    .build();
+            FirebaseApp.initializeApp(options);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/no/mesan/lunsjliste/resource/LunchListResource.java
+++ src/main/java/no/mesan/lunsjliste/resource/LunchListResource.java
@@ -1,5 +1,6 @@
 package no.mesan.lunsjliste.resource;
 
+import com.google.firebase.database.*;
 import no.mesan.lunsjliste.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/api/lunchlist", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -18,10 +21,30 @@ public class LunchListResource {
 
     @RequestMapping(value = "register", method = RequestMethod.POST)
     public ResponseEntity register(@RequestBody User user, @RequestParam String date) {
-        LOG.info("date: " + date);
         LocalDate localDate = LocalDate.parse(date, DateTimeFormatter.ISO_DATE);
+        String path = "/" + localDate.getYear() + "/" + localDate.getMonthValue() + "/" + localDate.getDayOfMonth();
 
-        LOG.info("localDateTime: " + localDate.toString());
+        DatabaseReference ref = FirebaseDatabase
+                .getInstance()
+                .getReference(path);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put(user.getUsername(), true);
+        ref.updateChildren(map);
+
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Map<String, Object> year = (HashMap<String, Object>) dataSnapshot.getValue();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
         return ResponseEntity.ok().build();
     }
 }
